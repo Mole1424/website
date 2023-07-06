558e2db57bc27932905555a62adc07da33a463a3
Mole1424 <joel@joelcoulon.com>
2023-07-06 23:24:53 +0000

    setting database path as environment variable

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from os import getenv
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DB_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-resetdb = False
+
+resetdb = True
 if resetdb:
     with app.app_context():
         db.drop_all()
